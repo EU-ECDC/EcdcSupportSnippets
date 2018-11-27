facfb3793435fa610d53b37cc81bfe563bf8b727
Maria <33788128+mariabnd@users.noreply.github.com>
2018-11-27 11:53:14 +0100

    Minor updates to code following use of style guide countries

--- R/convert_country.R
+++ R/convert_country.R
@@ -4,43 +4,29 @@
 #' format to another. It uses the list of 31 EU/EEA countries.
 #'
 #' @param name Country name to be converted
-#' @param type Can take the values "full", "ISO2", and "ISO3"
+#' @param type Can take the values "full" and "ISO2"
 #' @export
 #'
 convert_country <- function(name, type, ...){
-  if(!(type %in% c("full", "ISO2", "ISO3")))
+  if(!(type %in% c("full", "ISO2")))
     stop("incorrect type argument")
   if(type == "ISO2" && length(name) == 2)
     stop("name input incorrect length for conversion to ISO alpha 2 format")
-  if(type == "ISO2" && length(name) == 3)
-    stop("name input incorrect length for conversion to ISO alpha 3 format")
   # Load reference countries dataset
   #data("countries", package = ECDCpkg, envir = environment())
   data("countries", package = character(0), envir = environment())
   if(!(name %in% c(levels(countries[, 1]),
-                   levels(countries[, 2]),
-                   levels(countries[, 3]))))
+                   levels(countries[, 2]))))
     stop("name input not recognised")
 
   # Full name given
-  if(name %in% countries[, 1])
-    if(type == "ISO2")
-      out <- countries[which(countries[, 1] == name), 2]
-    if(type == "ISO3")
-      out <- countries[which(countries[, 1] == name), 3]
-  # ISO alpha 2 name given
   if(name %in% countries[, 2])
-    if(type == "full")
-      out <- countries[which(countries[, 2] == name), 1]
-    if(type == "ISO3")
-      out <- countries[which(countries[, 2] == name), 3]
-
-  # ISO alpha 3 name given
-  if(name %in% countries[, 3])
-    if(type == "full")
-      out <- countries[which(countries[, 3] == name), 1]
     if(type == "ISO2")
-      out <- countries[which(countries[, 3] == name), 2]
+      out <- countries[which(countries[, 2] == name), 1]
+  # ISO alpha 2 name given
+  if(name %in% countries[, 1])
+    if(type == "full")
+      out <- countries[which(countries[, 1] == name), 2]
 
     return(out)
 }
--- R/get_countries_from_interinstitutional_style_guide.R
+++ R/get_countries_from_interinstitutional_style_guide.R
@@ -2,12 +2,13 @@ library(XML)
 
 # All countries and codes
 annexA6 <- htmlParse("http://publications.europa.eu/code/en/en-5000600.htm")
-interinst_countries <- data.frame(readHTMLTable(getNodeSet(annexA6, "//table")[[1]]))
-names(interinst_countries)[1] <- "ISOalpha2"
+countries <- data.frame(readHTMLTable(getNodeSet(annexA6, "//table")[[1]]))
+names(countries) <- c("ISO2", "full")
+saveRDS(object = countries, file = "../data/countries.rds")
 
 # NUTS regions
 annexA10 <- htmlParse("http://publications.europa.eu/code/en/en-5001000.htm")
-interinst_regions <- data.frame(readHTMLTable(getNodeSet(annexA10, "//table")[[1]]))
-interinst_regions$ISOalpha2 <- substr(toupper(interinst_regions$Code),
+regions <- data.frame(readHTMLTable(getNodeSet(annexA10, "//table")[[1]]))
+regions$ISO2 <- substr(toupper(interinst_regions$Code),
                                       start = 1, stop = 2)
-interinst_regions <- interinst_regions[-which(is.na(interinst_regions$Name.in.English)), ]
+regions <- regions[- which(is.na(regions$Name.in.English)), ]
