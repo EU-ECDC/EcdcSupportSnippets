10b4ba81d9f35e3660c5633b4008d0648441873e
Maria <33788128+mariabnd@users.noreply.github.com>
2018-12-06 16:15:36 +0100

    Wrapped in a function

--- R/get_countries_from_interinstitutional_style_guide.R
+++ R/get_countries_from_interinstitutional_style_guide.R
@@ -1,14 +1,16 @@
 library(XML)
 
-# All countries and codes
-annexA6 <- htmlParse("http://publications.europa.eu/code/en/en-5000600.htm")
-countries <- data.frame(readHTMLTable(getNodeSet(annexA6, "//table")[[1]]))
-names(countries) <- c("ISO2", "full")
-saveRDS(object = countries, file = "../data/countries.rds")
-
-# NUTS regions
-annexA10 <- htmlParse("http://publications.europa.eu/code/en/en-5001000.htm")
-regions <- data.frame(readHTMLTable(getNodeSet(annexA10, "//table")[[1]]))
-regions$ISO2 <- substr(toupper(interinst_regions$Code),
-                                      start = 1, stop = 2)
-regions <- regions[- which(is.na(regions$Name.in.English)), ]
+get_countries <- function(){
+  # All countries and codes
+  annexA6 <- htmlParse("http://publications.europa.eu/code/en/en-5000600.htm")
+  countries <- data.frame(readHTMLTable(getNodeSet(annexA6, "//table")[[1]]))
+  names(countries) <- c("ISO2", "full")
+  saveRDS(object = countries, file = "../data/countries.rds")
+  
+  # NUTS regions
+  annexA10 <- htmlParse("http://publications.europa.eu/code/en/en-5001000.htm")
+  regions <- data.frame(readHTMLTable(getNodeSet(annexA10, "//table")[[1]]))
+  regions$ISO2 <- substr(toupper(interinst_regions$Code),
+                         start = 1, stop = 2)
+  regions <- regions[- which(is.na(regions$Name.in.English)), ]
+}
