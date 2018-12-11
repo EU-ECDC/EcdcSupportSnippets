335db2a6ca36aa3070999ce7aa91535435f814c3
Maria <33788128+mariabnd@users.noreply.github.com>
2018-12-11 15:53:35 +0100

    Fixed search in eurostat_dataset_and_info so it uses regex and added function to list names and codes of all datasets

--- R/eurostat_dataset_and_info.R
+++ R/eurostat_dataset_and_info.R
@@ -3,7 +3,13 @@ library(eurostat)
 eurostat_dataset_and_info <- function(dataset = "aact_ali01"){
   out <- list()
   out$data <- get_eurostat(id = dataset)
-  out$info <- search_eurostat(" ") %>% 
+  out$info <- search_eurostat("*", fixed = FALSE) %>% 
     filter(code == dataset)
   return(out)
+}
+
+list_all_sets <- function(){
+  print(search_eurostat("*", fixed = FALSE) %>% 
+          select(title, code) %>%
+          arrange(code), n = Inf)
 }
