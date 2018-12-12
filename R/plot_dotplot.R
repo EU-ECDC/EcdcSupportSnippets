f2e93e8671efc5d440ccb910400079d03d221ae6
Maria <33788128+mariabnd@users.noreply.github.com>
2018-12-12 16:08:50 +0100

    Turned into function

--- R/plot_dotplot.R
+++ R/plot_dotplot.R
@@ -3,19 +3,17 @@ library(dplyr) # For working with data
 library(ggplot2) # For plotting
 
 # Get data from dotplot
-p <- ggplot2::ggplot(data = cars,
-                     mapping = aes(x = factor(speed))) +
-  geom_dotplot()
-
-# Add factor labels to data
-plot_data <- dplyr::inner_join(layer_data(p, 1),
-                               tibble(group = unique(layer_data(p, 1)$group),
-                                      cat = levels(factor(cars$speed))),
-                               by = "group")
-
-# Plot points
-ggplot(data = plot_data,
-       mapping = aes(x = cat, y = countidx)) +
-  geom_point()
-
-#TODO: Turn into function
+dotplot <- function(data = cars, var = "speed"){
+  p <- ggplot2::ggplot(data = data,
+                       mapping = aes(x = factor(get(var)))) +
+    geom_dotplot()
+  # Add factor labels to data
+  plot_data <- dplyr::inner_join(layer_data(p, 1),
+                                 tibble(group = unique(layer_data(p, 1)$group),
+                                        cat = levels(factor(data[[var]]))),
+                                 by = "group")
+  # Plot points
+  ggplot(data = plot_data,
+         mapping = aes(x = cat, y = countidx)) +
+    geom_point()
+}
