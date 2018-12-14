67ddd411652f814008abb7d0fd300854a1f9e5ea
Maria <33788128+mariabnd@users.noreply.github.com>
2018-12-14 10:20:58 +0100

    Added function for finding most complete dataset based on time

--- R/eurostat_wrappers.R
+++ R/eurostat_wrappers.R
@@ -6,8 +6,10 @@
 #' @param dataset code of Eurostat dataset to retrieve
 #' @export
 #' @importFrom eurostat get_eurostat search_eurostat
-#' @import dplyr 
-eurostat_dataset_and_info <- function(dataset = "aact_ali01", ...){
+#' @import dplyr
+#' @examples
+#' data_and_info()
+data_and_info <- function(dataset = "aact_ali01", ...){
   out <- list()
   out$data <- get_eurostat(id = dataset)
   out$info <- search_eurostat("*", fixed = FALSE) %>% 
@@ -31,3 +33,43 @@ list_all_sets <- function(){
           select(title, code) %>%
           arrange(code), n = Inf)
 }
+
+#' Find dataset from year with least missings
+#' 
+#' @param dataset code of Eurostat dataset to retrieve
+#' @export
+#' @importFrom stringr str_replace_all
+#' @importFrom lubridate dmy as.duration
+#' @import dplyr
+#' @examples
+#' least_missings()
+least_missings <- function(dataset = "ilc_lvho05a", ...){
+  eur <- data_and_info(dataset = dataset)
+  data <- eur$data
+  info <- eur$info
+  update <- info$`last update of data`
+  update <- dmy(str_replace_all(string = update, 
+                                pattern = "[.]", replacement = "-"))
+  # Find time with least missing
+  time_vals <- unique(data$time)
+  mis <- c()
+  for (i in 1:length(time_vals)){
+    mis[i] <- sum(is.na(data %>% filter(time == time_vals[i])))
+  }
+  opt_time <- time_vals[which.min(mis)]
+  # How far is this from recent update (in days)?
+  elapsed <- as.duration(opt_time %--% update) / ddays(1)
+  data <- data %>% filter(time == opt_time)
+  mis_full <- (sum(is.na(data)) / dim(data)[1]) * 100
+  mis_col <- (colSums(is.na(data)) / dim(data)[1]) * 100
+  # Save outputs
+  out <- list(times = list(optimal = opt_time,
+                           last_updated = update,
+                           days_elapsed = elapsed),
+              percentage_missing = list(overall = mis_full,
+                                        by_vars = mis_col),
+              data = data)
+  return(out)
+}
+# TODO: Consider option for outputint plot object from visdat
+# TODO: Replace the loop with something faster
